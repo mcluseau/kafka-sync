8060b96205fee81c221950b06261d11c8c6eec72
Mikaël Cluseau <mcluseau@isi.nc>
2019-02-17 18:52:53 +1100

    more log

--- kafka-sync.go
+++ kafka-sync.go
@@ -234,7 +234,7 @@ func (s *Syncer) IndexTopic(kafka sarama.Client, index diff.Index) (msgCount uin
 		glog.V(4).Info("-> resume offset: ", resumeOffset)
 
 		if resumeOffset+1 >= highWater {
-			glog.V(4).Info("-> would consume from %d, high water is %d, so we're up-to-date",
+			glog.V(4).Infof("-> would consume from %d, high water is %d, so we're up-to-date",
 				resumeOffset+1, highWater)
 			return
 		}
